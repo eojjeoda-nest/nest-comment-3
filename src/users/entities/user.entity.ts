92e2396e9f6eccac2096f6cf50c36f208ca47fe2
jonghun <minn4072@gmail.com>
2023-12-07 09:22:02 +0900

    style : user enitity class name 수정

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -2,7 +2,7 @@ import { CommonEntity } from 'src/common/entities/common.entity';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class User extends CommonEntity {
+export class UserEntity extends CommonEntity {
   @PrimaryGeneratedColumn()
   primaryUserId: number;
 
